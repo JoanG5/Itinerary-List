33326dc65109dc032be4bef3b4e6bb81772c52f7
Joan <joanguzman553@gmail.com>
2024-11-06 14:10:22 -0500

    added button for sign in with google and removed extra text

--- app/comp/SignIn.js
+++ app/comp/SignIn.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { auth } from "../firebase/config";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { Button } from "@/components/ui/button";
 
 function SignIn() {
   const signInWithGoogle = async () => {
@@ -14,12 +15,9 @@ function SignIn() {
 
   return (
     <>
-      <button className="sign-in" onClick={signInWithGoogle}>
+      <Button className="sign-in" onClick={signInWithGoogle}>
         Sign in with Google
-      </button>
-      <p>
-        Do not violate the community guidelines or you will be banned for life!
-      </p>
+      </Button>
     </>
   );
 }
